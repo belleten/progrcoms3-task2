c7804fa9a85e587c6cfa369116ab7ef3c597d28d
belleten <albert.rb@gmail.com>
2016-04-20 11:57:49 +0000

    modified forms and main

--- mainpage.py
+++ mainpage.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-def ins_user(username, fullname, email, password)
+def ins_user(username, fullname, email, password):
 	conn = sqlite3.connect('userdatabase.db')
 	try:
 		conn.execute("insert into login (username, fullname, email, password) values (?, ?, ?, ?)",(username, fullname, email, password))
@@ -14,7 +14,7 @@ def ins_user(username, fullname, email, password)
 	except:
 		return False
 
-def get_users():
+def get_users(username, fullname, email, password):
 	conn = sqlite3.connect('userdatabase.db')
 	cursor = conn.execute("SELECT username, fullname, email, password")
 	data = [row for row in cursor]
@@ -35,4 +35,24 @@ def new_user():
 		fullname = request.form.get('fullname')
 		email = request.form.get('email')
 		password = request.form.get('password')
+		if ins_user(username, fullname, email, password):
+			return redirect(url_for('main'))
+			#return redirect(url_for('newregister.html')
+		else:
+			return "Error inserting temperature"		
 
+#@app.route('/new_register', methods=['POST'])
+#def new_reg():
+#	return render_template('newregister.html')
+#	if request.method == 'POST':
+
+
+@app.route('/show_users', methods=['GET','POST'])
+def list_users():
+	table_users = get_users(username, fullname, email, password)
+	return render_template('list_users_table.html', table_users=table_users)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run("0.0.0.0")
