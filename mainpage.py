c53ce491a96f549e99b6d17c45876845ff9ca195
belleten <albert.rb@gmail.com>
2016-04-21 12:22:37 +0000

    new and modified templates/ modified mainpage.py also commented

--- mainpage.py
+++ mainpage.py
@@ -5,91 +5,71 @@ from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-def ins_user(username, fullname, email, password):
+def ins_user(username, fullname, email, password):  #function used to insert new users to the database
 	conn = sqlite3.connect('userdatabase.db')
-	try:
-		conn.execute("insert into login (username, fullname, email, password) values (?, ?, ?, ?)",(username, fullname, email, password))
-		conn.commit()
+	try:   #here we try to insert the parameters values of new users into database, if done OK returns TRUE
+		conn.execute("insert into login (username, fullname, email, password) values (?, ?, ?, ?)",(username, fullname, email, password))	
+		conn.commit() 
 		conn.close()
-		return True
-	except:
+		return True  
+	except:  #if the insert fails it returns FALSE
 		return False
 
-def get_users():
+def get_users():    #function used to find users already in database
 	conn = sqlite3.connect('userdatabase.db')
-	cursor = conn.execute("SELECT username, fullname, email, password from login")
-	data = [row for row in cursor]
+	cursor = conn.execute("SELECT username, fullname, email, password from login") #we select all values of each user from table named login
+	data = [row for row in cursor] #data saves info for each row of table login
 	conn.close()
 	return data
 
-def check_password(username_login,password_login):
-	# agafar dades (password) de la BD pwer (WHERE) usuari usr
+def check_password(username_login,password_login): #function that checks if the username and password are already into database
 	conn = sqlite3.connect('userdatabase.db')
-	cursor = conn.execute("SELECT password from login WHERE username = ?",(username_login,))
-	db_password =  cursor.fetchone()
+	cursor = conn.execute("SELECT password from login WHERE username = ?",(username_login,)) #here we select the password that correspond to the username passed by the user 
+	db_password =  cursor.fetchone() #this will give the lenght of password
 	print(db_password)
-	if db_password is None:
+	if db_password is None: #if the username doesn't exist, the database will tell that the lenght is less than 0 because it can't find a password matching a nonexisting username
 		return False
+	else:  #but if the username exists in the database it will give the value to db_password [0], meaning that we want the first into the list
+		db_password = db_password[0] 
+	if password_login == db_password: 
+		return True   #if also the password given by the user is the same that we have into database it returns TRUE
 	else:
-		db_password = db_password[0]
-	if password_login == db_password:
-		return True
-	else:
-		return False 
+		return False  #if not FALSE
 
 @app.route('/')
-def main():
+def main():  #defines the landing page into the specified route
 	return render_template('landingpage.html')
 
 @app.route('/insert_user', methods=['GET','POST'])
-def new_user():
-	if request.method == 'GET':
-		return render_template('user_register_form.html')
-	elif request.method == 'POST':
-		username = request.form.get('username')
+def new_user():  #function that uses ins_user function and get and post methods into a html code to insert new users into database
+	if request.method == 'GET':   
+		return render_template('user_register_form.html') #it shows the html with the form to create new users ready to get the info
+	elif request.method == 'POST': 
+		username = request.form.get('username') 
 		fullname = request.form.get('fullname')
 		email = request.form.get('email')
 		password = request.form.get('password')
-		if ins_user(username, fullname, email, password):
-			return redirect(url_for('main'))
-			#return redirect(url_for('newregister.html')
-		else:
-			return "Error inserting temperature"		
+		if ins_user(username, fullname, email, password):  
+			return render_template('newregister.html')	
 
-#@app.route('/new_register', methods=['POST'])
-#def new_reg():
-#	return render_template('newregister.html')
-#	if request.method == 'POST':
-
-
-@app.route('/show_users', methods=['GET','POST'])
-def list_users():
+@app.route('/show_users')
+def list_users():  #list an html page that shows all users into database
 	table_users = get_users()
 	return render_template('list_users_table.html', table_users=table_users)
 
 
 @app.route('/login', methods=['GET','POST'])
-def login_user():
+def login_user():   #function to get the login info provided by the user and looking for it inside database data using get and post methods
 	if request.method == 'GET':
-		return render_template('login_page.html')
+		return render_template('login_page.html') #it gets the info using a html page
 	elif request.method == 'POST':
-		username_login = request.form.get('username')
+		username_login = request.form.get('username')  
 		password_login = request.form.get('password')
-		if check_password(username_login,password_login):
-			#return "WELCOME BACK!!!!!"
-			#return redirect(url_for('ret_main'))
-			return render_template('wel_back.html')
+		if check_password(username_login,password_login): #uses check_password function to check if the username and password already exist inside database
+			return render_template('wel_back.html') #if TRUE welcome back html page
 		else:
-			#return "ERROR --> INVALID username OR password"
-			return render_template('invalid_log.html')
+			return render_template('invalid_log.html') #if FALSE incorrect login html page
 
-#@app.route('/back', methods=['GET','POST'])
-#def ret_main():
-#	if request.method == 'GET':
-#		return render_template('wel_back.html')
-#	elif request.method == 'POST':
-#		return redirect(url_for('main'))
-
-if __name__ == '__main__':
+if __name__ == '__main__': #for inicialize webapp
     app.debug = True
     app.run("0.0.0.0")
